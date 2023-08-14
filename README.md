33745571e4c9b16a2ed8accdeec7f90d5bad8cff
Anesh SM <aneshsm204@gmail.com>
2023-08-15 01:45:51 +0530

    Added Syntax checker,Formatter and Tester configurations

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # restful_API-PostgreSQL_Sequelize
 
-Restful API project which is built using Node.js Express.js Typescript Sequelize with PostgreSQL as Relational Database
+Restful API project which is built using Node.js Express.js Typescript Sequelize with PostgreSQL as Relational Database along with Formatter,Syntax checker and Tester configuration
+
+npx tsc --init
+npx eslint --init
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import express,{Express,Request,Response} from 'express'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv-safe';
+
+import add from '@src/add/add';
 
 //* Loads all variables defined in the .env to node environment process.env
-dotenv.config()
+dotenv.config();
 
-const PORT=process.env.PORT || 3000
-const app:Express=express()
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req:Request,res:Response)=>{
-    res.send('<h1>Hi Express TypeScript Backend was Successful</h1>')
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hi Express TypeScript Backend was Successful</h1>');
+});
 
-app.listen(PORT,()=>console.log(`Running on http://localhost:${PORT}`))
+console.log(add(1, 4));
+
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
